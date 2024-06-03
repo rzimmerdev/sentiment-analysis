0b02d92db0fa0778b09ca46e7bdaa02fb931d7b6
Murilo Soave <37509039+murilosoave@users.noreply.github.com>
2024-06-03 14:56:15 -0300

    refact: Add parameters to control what model must be used in training

--- src/train.py
+++ src/train.py
@@ -1,38 +1,52 @@
+import json
 import os
 
 from lightning import Trainer
 from lightning.pytorch.loggers import CSVLogger
 from transformers import BertModel
 
-from model import LitPhraseClassifier
+from model import LitPhraseClassifier, LitBoWClassifier, LitLSTMClassifier
 from dataset import FinancialPhraseDataset
 
 
-def train(model_path=None, max_epochs=5):
+def train(model_name=None, model_parameters=None, model_path=None, max_epochs=5):
     dataset = FinancialPhraseDataset()
     train_loader, val_loader = dataset.get_data_loaders(batch_size=8, num_workers=4, train_size=0.9)
 
-    bert = BertModel.from_pretrained('bert-base-uncased')
+    model = load_model(model_name=model_name
+                       model_parameters=model_parameters)
 
     if model_path is not None:
         model_path = model_path + '.ckpt'
         try:
-            lit_model = LitPhraseClassifier.load_from_checkpoint(model_path, bert=bert)
+            model = model.load_from_checkpoint(model_path, bert=bert)
         except Exception as e:
             print(f"Error loading model: {e}")
             print("Training from scratch...")
-            lit_model = LitPhraseClassifier(bert=bert)
     else:
         model_path = 'lit-phrases-bert' + '.ckpt'
-        lit_model = LitPhraseClassifier(bert=bert)
 
     log_dir = os.path.join('logs/sentiment-analysis', 'version_0')
     logger = CSVLogger("logs", name="sentiment-analysis", version=len(os.listdir(log_dir)))
     trainer = Trainer(max_epochs=max_epochs, logger=logger)
-    trainer.fit(model=lit_model, train_dataloaders=train_loader, val_dataloaders=val_loader)
+    trainer.fit(model=model, train_dataloaders=train_loader, val_dataloaders=val_loader)
 
     # save model
     trainer.save_checkpoint(model_path)
+
+
+def load_model(model_name, model_parameters, pre_trained):
+    if model_name == 'LitBoWClassifier':
+        model = LitBoWClassifier(**model_parameters)
+    elif model_name == 'LitLSTMClassifier':
+        model = LitLSTMClassifier(**model_parameters)
+    elif model_name == 'LitPhraseClassifier':
+        bert = BertModel.from_pretrained(args.pre_trained)
+        model = LitPhraseClassifier(bert=bert, **model_parameters)
+    else:
+        raise ValueError("Unknown model type")
+
+    return model
 
 
 if __name__ == '__main__':
@@ -40,7 +54,12 @@ if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser(description='Train a sentiment analysis model')
+    parser.add_argument('--model', type=str, help='Model architecture', default='LitPhraseClassifier')
+    parser.add_argument('--model-parameters', type=json.loads, help='Model hyperparameters', default='{}')
     parser.add_argument('--pre-trained', type=str, help='Path to a pre-trained model', default=None)
     parser.add_argument('--max-epochs', type=int, help='Maximum number of epochs', default=5)
     args = parser.parse_args()
-    train(args.pre_trained, args.max_epochs)
+    train(model_name=args.model,
+          model_parameters=args.model_parameters,
+          model_path=args.pre_trained,
+          max_epochs=args.max_epochs)
